8c63cf96bb84694149a3eb4cb21ef77534cf5cfb
Migue <migueguillamon@gmail.com>
2014-04-10 16:21:42 +0200

    Cull tickets when project gets destroyed

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,5 +4,15 @@ class Project < ActiveRecord::Base
 	validates :name, presence: true
 
 	# Relationships
-	has_many :tickets
+	# there are 3 options in wich we can define what rails should do when you delete a model with dependencies
+	#  :destroy 
+	# 		when you delete a project, it will call the "destroy" method on every associated ticket
+	# 		if the tickets have callback operations on destroy, then you should use this option
+	#  :delete_all
+	# 		when you delete a project, it will delete all the tickets associated with a simple sql statement
+	# 		(it's a quicker operation than the "destroy" option)
+	#  :nullify
+	# 		as you can imagine, this only unassociate the tickets related to the project and set them to nil
+	# 		note that the tickets will stay undeleted
+	has_many :tickets, dependent: :delete_all
 end

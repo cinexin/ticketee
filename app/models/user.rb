b116ddc6faa6bbbd8dc86cdc2792e482853656e3
Migue <migueguillamon@gmail.com>
2014-04-28 17:14:08 +0200

    Added creating, editing, updating and deleting tickets to assigning permissions interface

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ActiveRecord::Base
 	# we decided to override the default "to_s" method 
 	# view the "app/views/admin/users/index.html.erb" to find out why
 	def to_s
-	  puts "The user string: " + "#{email} (#{admin? ? "Admin" : "User"})"
 	  "#{email} (#{admin? ? "Admin" : "User"})"
 	end
 end
